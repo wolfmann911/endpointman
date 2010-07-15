54b1bbc02092c009f5b660e398e973934d93f3e3
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-07-15 17:16:59 +0000

    removes sip_notify.conf and uses core_conf to generate, should probably move data into xml files and derive but this is first step in the transition to avoid conflicts with restart module. Only works on 2.8+, we can backport the core changes to 2.7 if needed so it works there also

--- functions.inc.php
+++ functions.inc.php
@@ -1,7 +1,22 @@
 <?PHP
-/*
-This File is placed here for FreePBX Module Purposes. FreePBX calls unpon this module everytime FreePBX is reloaded.
-
-Thoughts are to perhaps allow phone reboots and rebuilds when the user reloads freepbx configuration
-*/
-?>
+function endpointman_get_config($engine) {
+  global $db;
+  global $ext; 
+  global $core_conf;
+  switch($engine) {
+    case "asterisk":
+	    if (isset($core_conf) && is_a($core_conf, "core_conf")) {
+        $core_conf->addSipNotify('polycom-check-cfg',array('Event' => 'check-sync','Content-Length' => '0'));
+        $core_conf->addSipNotify('polycom-reboot',array('Event' => 'check-sync','Content-Length' => '0'));
+        $core_conf->addSipNotify('sipura-check-cfg',array('Event' => 'resync','Content-Length' => '0'));
+        $core_conf->addSipNotify('grandstream-check-cfg',array('Event' => 'sys-control'));
+        $core_conf->addSipNotify('cisco-check-cfg',array('Event' => 'check-sync','Content-Length' => '0'));
+        $core_conf->addSipNotify('reboot-snom',array('Event' => 'reboot','Content-Length' => '0'));
+        $core_conf->addSipNotify('aastra-check-cfg',array('Event' => 'check-sync','Content-Length' => '0'));
+        $core_conf->addSipNotify('linksys-cold-restart',array('Event' => 'reboot_now','Content-Length' => '0'));
+        $core_conf->addSipNotify('linksys-warm-restart',array('Event' => 'restart_now','Content-Length' => '0'));
+        $core_conf->addSipNotify('spa-reboot',array('Event' => 'reboot','Content-Length' => '0'));
+      }
+    break;
+  }
+}
