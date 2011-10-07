a5771d17600ddd18aba8085be562a1f912a8e698
tm1000 <tm1000@05cd7311-dd0d-0410-8c4e-c7a75d82e046>
2011-10-07 17:06:26 +0000

    Fixed errors in jsonwrapper
    git-svn-id: http://www.freepbx.org/v2/svn/modules/branches/2.10/endpointman/@12779 05cd7311-dd0d-0410-8c4e-c7a75d82e046

--- includes/jsonwrapper_inner.php
+++ includes/jsonwrapper_inner.php
@@ -1,23 +1,24 @@
 <?php
-
-require_once 'JSON/JSON.php';
-
-function json_encode($arg)
-{
-	global $services_json;
-	if (!isset($services_json)) {
-		$services_json = new Services_JSON();
+if(!function_exists('json_encode')){
+	require_once 'JSON/JSON.php';
+	function json_encode($arg)
+	{
+		global $services_json;
+		if (!isset($services_json)) {
+			$services_json = new Services_JSON();
+		}
+		return $services_json->encode($arg);
 	}
-	return $services_json->encode($arg);
 }
 
-function json_decode($arg)
-{
-	global $services_json;
-	if (!isset($services_json)) {
-		$services_json = new Services_JSON();
+if(!function_exists('json_decode')){
+	require_once 'JSON/JSON.php';
+	function json_decode($arg)
+	{	
+		global $services_json;
+		if (!isset($services_json)) {
+			$services_json = new Services_JSON();
+		}
+		return $services_json->decode($arg);
 	}
-	return $services_json->decode($arg);
 }
-
-?>
