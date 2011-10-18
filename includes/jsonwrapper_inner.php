4c53b18940e0c995cf4e5fc920d919b37fffb0dd
Andrew Nagy <andrew.nagy@the159.com>
2011-10-18 15:59:31 -0700

    Revert "Fixed errors in jsonwrapper"
    This reverts commit a5771d17600ddd18aba8085be562a1f912a8e698.

--- includes/jsonwrapper_inner.php
+++ includes/jsonwrapper_inner.php
@@ -1,24 +1,23 @@
 <?php
-if(!function_exists('json_encode')){
-	require_once 'JSON/JSON.php';
-	function json_encode($arg)
-	{
-		global $services_json;
-		if (!isset($services_json)) {
-			$services_json = new Services_JSON();
-		}
-		return $services_json->encode($arg);
+
+require_once 'JSON/JSON.php';
+
+function json_encode($arg)
+{
+	global $services_json;
+	if (!isset($services_json)) {
+		$services_json = new Services_JSON();
 	}
+	return $services_json->encode($arg);
 }
 
-if(!function_exists('json_decode')){
-	require_once 'JSON/JSON.php';
-	function json_decode($arg)
-	{	
-		global $services_json;
-		if (!isset($services_json)) {
-			$services_json = new Services_JSON();
-		}
-		return $services_json->decode($arg);
+function json_decode($arg)
+{
+	global $services_json;
+	if (!isset($services_json)) {
+		$services_json = new Services_JSON();
 	}
+	return $services_json->decode($arg);
 }
+
+?>
