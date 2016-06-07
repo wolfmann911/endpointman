de7b90b961e941a80236e6a27b2b96b42ecb93af
vsc55 <jpastor@cerebelum.net>
2016-06-07 16:24:22 +0200

    Update CSS Plugin JQuery (Liquid Slider), Fix Bug RightNav, continue update...

--- views/epm_templates/editor.views.rnav.php
+++ views/epm_templates/editor.views.rnav.php
@@ -1,16 +1,15 @@
 <?php
 	if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 ?>
-<br />
-<div id="toolbar-all-side">
-	<a href="config.php?display=epm_templates" class="btn btn-default btn-block"><i class="fa fa-list"></i>&nbsp; <?php echo _("List Tempalte Custom") ?></a>
+<div id="toolbar-qrnav">
+	<a href="config.php?display=epm_templates" class="btn btn-default"><i class="fa fa-list"></i>&nbsp; <?php echo _("List Tempalte Custom") ?></a>
 </div>
 <table 
 	data-url="ajax.php?module=endpointman&amp;module_sec=epm_templates&amp;module_tab=manager&amp;command=list_current_template"
 	data-cache="false"
     data-toolbar="#toolbar-qrnav"
     data-toggle="table"
-    data-search="false" 
+    data-search="true" 
     class="table table-hover"
 	id="table-all-side">
 	<thead>
