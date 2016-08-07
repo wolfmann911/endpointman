2ec1363d14a13e1a79380237e6837ecfd05695af
vsc55 <jpastor@cerebelum.net>
2016-08-07 13:10:11 +0200

    Add CSS Fix Bug bootstrap-fix.css.
    Update bootstrap-select.

--- views/epm_config/manager.list.hide.php
+++ views/epm_config/manager.list.hide.php
@@ -5,7 +5,7 @@
 <div class="alert alert-info" role="alert">
 	<h3><?php echo _("Packages Hiden") ?></h3>
     <div class="input-group">
-        <span class="input-group-addon" data-for="basic-addon1">Marcas/Modelos Ocultos:</span>
+        <span class="input-group-addon">Marcas/Modelos Ocultos:</span>
         <select 
 			data-url="ajax.php?module=endpointman&amp;module_sec=epm_config&amp;module_tab=manager&amp;command=list_brand_model_hide"
             data-cache="false"
@@ -13,13 +13,15 @@
 			data-label = ""
             data-selected-text-format="count > 4"
             data-size="10"
-            data-style="btn-info" 
+            data-style="" 
             data-live-search-placeholder="Buscar..." 
             data-live-search="true" 
-            class="form-control selectpicker show-tick" 
+            data-done-button="true"
+            class="selectpicker show-tick form-control"
             id="epm_config_manager_select_hidens" 
             name="epm_config_manager_select_hidens"
             multiple>
         </select>
+        <span class="input-group-addon"><span class="help"><i class="fa fa-question-circle"></i><span style="display: none;">Esta lista contiene todos las Marcas/Productos/Modelos que han sido <br /> ocultados. Hacido click en cualquiera de ellos lo recuperaras.</span></span></span>
     </div>
 </div>
