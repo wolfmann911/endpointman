e57d19336344886b93287a9666487c63161c7120
Stefano Fancello <stefano.fancello@nethesis.it>
2018-01-17 09:46:54 +0100

    Fix selected in template editor dropdown

--- views/epm_templates/editor.views.template.php
+++ views/epm_templates/editor.views.template.php
@@ -353,8 +353,7 @@
 	                    									echo '<select name="'.$srow['key'].'" id="'.$srow['key'].'" class="form-control selectpicker show-tick" data-style="" data-live-search-placeholder="Buscar" data-live-search="true">';
 															foreach($srow['data'] as $lrow) 
 															{
-																//echo '<option value="'.$lrow['value'].'" '.(array_key_exists('selected',$lrow['selected'])? $lrow['value'] : '').' >'.$lrow['description'].'</option>';
-																echo '<option value="'.$lrow['value'].'" '.($lrow['value'] == $lrow['selected'] ? 'selected' : '').' >'.$lrow['description'].'</option>';
+																echo '<option value="'.$lrow['value'].'" '.(array_key_exists('selected',$lrow)? 'selected' : '').' >'.$lrow['description'].'</option>';
 															}
 															echo '</select>';
 															?>
